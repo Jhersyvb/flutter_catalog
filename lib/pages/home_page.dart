4b8438a1fc06815f5b6e78aca22945cfb716d329
Jhersy <jhersyvb@gmail.com>
2021-09-21 19:25:19 -0500

    Day 13 completed: Local Files | Load & Decode JSON

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,32 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
 import 'package:flutter_catalog/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int days = 30;
+
   final String name = "Sara";
 
-  const HomePage({Key? key}) : super(key: key);
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson = await rootBundle.loadString('assets/files/catalog.json');
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData['products'];
+    print(productsData);
+  }
 
   @override
   Widget build(BuildContext context) {

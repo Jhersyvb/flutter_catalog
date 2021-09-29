4f637b9c50f961a205b46e627abcfad2d4998697
Jhersy <jhersyvb@gmail.com>
2021-09-29 10:45:20 -0500

    Day 22 completed: Placeholder | Cart Page

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -12,6 +12,60 @@ class CartPage extends StatelessWidget {
         backgroundColor: Colors.transparent,
         title: 'Cart'.text.make(),
       ),
+      body: Column(
+        children: [
+          // Placeholder().p32().expand(),
+          CartList().p32().expand(),
+          Divider(),
+          CartTotal(),
+        ],
+      ),
+    );
+  }
+}
+
+class CartTotal extends StatelessWidget {
+  const CartTotal({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          '\$ 9999'.text.xl5.make(),
+          30.widthBox,
+          ElevatedButton(
+            onPressed: () {},
+            child: 'Buy'.text.make(),
+          ).w32(context)
+        ],
+      ),
+    );
+  }
+}
+
+class CartList extends StatefulWidget {
+  const CartList({Key? key}) : super(key: key);
+
+  @override
+  State<CartList> createState() => _CartListState();
+}
+
+class _CartListState extends State<CartList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context, index) => ListTile(
+        leading: Icon(Icons.done),
+        trailing: IconButton(
+          icon: Icon(Icons.remove_circle_outline),
+          onPressed: () {},
+        ),
+        title: 'Item 1'.text.make(),
+      ),
     );
   }
 }
